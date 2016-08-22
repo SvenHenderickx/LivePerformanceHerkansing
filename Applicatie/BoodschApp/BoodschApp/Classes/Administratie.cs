4ea051b74e002feec7900b5a9f8076b276b8052a
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-08-22 16:26:56 +0200

    Klaar omzetten naar code

--- Applicatie/BoodschApp/BoodschApp/Classes/Administratie.cs
+++ Applicatie/BoodschApp/BoodschApp/Classes/Administratie.cs
@@ -8,7 +8,46 @@ namespace BoodschApp.Classes
 {
     public static class Administratie
     {
-        public Boodschappenlijst Boodschappenlijst { get; set; }
+        public static Boodschappenlijst Boodschappenlijst { get; set; }
+        public static List<Winkel> Winkels { get; set; }
+        public static List<Gerecht> Gerechten { get; set; }
+        public static List<Product> Producten { get; set; }
 
+        //Methods
+
+        /// <summary>
+        /// Voegt gerecht toe aan de lijst van gerechten
+        /// </summary>
+        /// <param name="gerecht"></param>
+        public static void GerechtToevoegen(Gerecht gerecht)
+        {
+            //
+        }
+
+        /// <summary>
+        /// Voegt een winkel toe aan de lijst van winkels
+        /// </summary>
+        /// <param name="winkel"></param>
+        public static void WinkelToevoegen(Winkel winkel)
+        {
+            //
+        }
+
+        /// <summary>
+        /// Voegt een product toe aan de lijst van producten
+        /// </summary>
+        /// <param name="product"></param>
+        public static void ProductToevoegen(Product product)
+        {
+            //
+        }
+
+        /// <summary>
+        /// Geeft een overzicht van de combinaties van gerechten die samen het minste resten over laten
+        /// </summary>
+        public static List<string> SorterenWeinigRestjes()
+        {
+            return null;
+        }
     }
 }
